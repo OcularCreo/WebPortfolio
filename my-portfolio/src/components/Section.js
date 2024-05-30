776d730241276e9b9db98f9e77a16ab2e9721a1c
OcularCreo <122617813+OcularCreo@users.noreply.github.com>
2024-05-30 00:51:48 -0400

    made titles responsive and cleaned up some html

--- my-portfolio/src/components/Section.js
+++ my-portfolio/src/components/Section.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "../styles/Home.css";
 
 export const Section = ({imageURL, setHeight}) =>{
     return (
-        <div></div>
+        
+        <div className="img-section">
+            <div className="section-overlay">
+
+            </div>
+        </div>
     );
 }
--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -1,36 +1,36 @@
 import React from "react";
 import { Button, Container, Row, Col } from "react-bootstrap";
-import { NavBar } from '../components/NavBar';
 import { CustomBtn } from "../components/CustomBtn";
-import myImage from '../assets/images/4.jpg';
+import "../styles/Home.css";
+
+import testImg from "../assets/images/4.jpg";
 
 export const Home = () =>{
     return(
-        <div style={{overflow: "hidden"}}>
-            
-            <div className="section-container home-section">
-                
-                <img src={myImage} className="section-img"></img>
-                <div className="section-overlay d-flex align-items-center">
-                    
+        <div>
+
+            {/* Section 1: Home */}
+            <div className="img-section home-section">                          {/* BG image container */}
+                <div className="section-overlay d-flex align-items-center">     {/* Image overlay Container */}
                     <Container>
-                        <h1 className="title-1" style={{fontSize: "4rem"}}>JORDAN COOLIGAN PANG</h1>
-                        <h3 className="title-2" style={{fontSize: "2.5rem"}}>Multimedia Developer & Designer</h3>
+                        
+                        {/* Titles headers */}
+                        <h1 className="name-title">Jordan Cooligan Pang</h1>
+                        <h2 className="descr-title">Multimedia Developer & Designer</h2>
+                        
+                        {/* Buttons Container */}
                         <Container className="mt-5">
-                            <CustomBtn btnTxt="Resume" className=""/>
+                            <CustomBtn btnTxt="Resume"/>
                             <CustomBtn btnTxt="Portfolio"/>
                         </Container>
-                        
                     </Container>
                 </div>
             </div>
 
             {/* Section 2: About me */}
-            <div className="section-about">
-                <div className="section-overlay">
-                    
-                    <p>test stuff hehe</p>
-
+            <div className="img-section about-section">      {/* BG image container */}                                     
+                <div className="section-overlay">            {/* Image overlay container */}
+                
                 </div>
             </div>
 
