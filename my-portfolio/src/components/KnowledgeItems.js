cf99672b153c059304e1e0dc45e4f27f14747b85
OcularCreo <122617813+OcularCreo@users.noreply.github.com>
2024-06-14 00:05:36 -0400

    responsive skills section in progress

--- my-portfolio/src/components/KnowledgeItems.js
+++ my-portfolio/src/components/KnowledgeItems.js
@@ -1,15 +1,44 @@
+import { useEffect, useState } from "react";
 import '../styles/KnowledgeItems.css'
 
 //typical format is title, icon, and description
 const KnowledgeItems = (props) =>{
+
+    const [isMobileScreen, setIsMobileScreen] = useState(window.innerWidth <= 600); //check if the current width is mobile width
+
+    useEffect(() =>{
+
+        const handleMobileResize = () =>{
+            setIsMobileScreen(window.innerWidth <= 600);
+        }
+
+        window.addEventListener("resize", handleMobileResize); 
+
+        return () =>{
+            window.removeEventListener("resize", handleMobileResize);
+        }
+
+    }, []);
 
     const items = props.items; 
 
     return items.map((item) => (
         <div className="knowledge-wrapper">
             <p className="icon">{item.icon}</p>
-            <h1 className="title">{item.title}</h1>
-            <p className="desc">{item.description}</p>
+            {isMobileScreen ? 
+            <>
+                <div className="mobile-wrapper">
+                    <h1 className="title">{item.title}</h1>
+                    <p className="desc">{item.description}</p>
+                </div>
+            </>
+            : 
+            <>
+                <h1 className="title">{item.title}</h1>
+                <p className="desc">{item.description}</p>
+            </>
+            }
+            
         </div>
     ))
 
